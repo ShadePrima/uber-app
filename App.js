e9d9f6e9ddf15a7942be26dfe57ee1efadd18b35
ShadePrima <aleksandrag1984@gmail.com>
2022-09-03 15:22:29 +0300

    add navigation

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
+import "react-native-gesture-handler";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
-import HomeScreen from "./screens/HomeScreen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "./screens/HomeScreen";
+import MapScreen from "./screens/MapScreen";
+
 import { store } from "./store";
+import EatsScreen from "./screens/EatsScreen";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="EatsScreen"
+              component={EatsScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+          {/* <HomeScreen /> */}
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,7 +3,7 @@ import { View, Text, SafeAreaView, StyleSheet, Image } from "react-native";
 import tw from "twrnc";
 import NavOptions from "../components/NavOptions";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
